f1443260d87f280f6e7d01bba9545cd632242059
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2023-07-19 10:31:07 +0300

    config

--- src/com/patikadev/Helper/Config.java
+++ src/com/patikadev/Helper/Config.java
@@ -4,6 +4,6 @@ public class Config {
     public static final String PROJECT_TITLE = "Patika.Dev";
     public static final String DB_URL = "jdbc:mysql://localhost/patika";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "okanokan1";
+    public static final String DB_PASSWORD = "******";
 
 }
